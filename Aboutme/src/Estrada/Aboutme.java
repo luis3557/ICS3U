f54bafd390893fea490fb79c81e70ee8457709c6
Luis Estrada <thelast3557@gmail.com>
2016-09-14 12:40:24 -0400

    AboutMe

---
+++ Aboutme/src/Estrada/Aboutme.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ *IC3U
+ *Aboutme
+ *Luis Estrada
+ *September 12
+ */
+public class Aboutme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%10s","Luis.E\n");
+		System.out.format("%10s","Miss.K\n" );
+		System.out.format("%10s","Runnymede Collegiate Institute\n");
+		System.out.format("%10s","\"Go Ravens!/");
+	}
+
+}
