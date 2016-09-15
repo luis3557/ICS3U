6e5dfd8bf0747000c21a5f49f4ee7baeb98153fa
Luis Estrada <thelast3557@gmail.com>
2016-09-15 13:30:50 -0400

    BingoCard

---
+++ Aboutme/src/Estrada/BingoCard.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ * @author 344169792
+ *
+ */
+public class BingoCard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-10s%8s%8s%8s%8s","B","I","N","G","\tO\n");
+		System.out.format("%-10s%8s%8s%8s%8s","2","20","42","60","\t64\n");
+		System.out.format("%-10s%8s%8s%8s%8s","14","25","32","55","\t70\n");
+		System.out.format("%-10s%8s%8s%8s%8s","5","18","FREE","53","\t67\n");
+		System.out.format("%-10s%8s%8s%8s%8s","12","16","31","46","\t75\n");
+		System.out.format("%-10s%8s%8s%8s%8s","10","22","39","59","\t71\n");
+
+	}
+
+}
--- Aboutme/src/Estrada/ClassSchedule.java
+++ Aboutme/src/Estrada/ClassSchedule.java
@@ -15,8 +15,8 @@ public class ClassSchedule {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.format("%-10s%-12s%-10s","Period","Day1","Day2\n");
-		System.out.format("%-8s%-12s%-12s","1","ENG3C1-01\t","ENG3C1-01\n");
+		System.out.format("%-10s%10s%10s","Period","Day1","Day2\n");
+		System.out.format("%-10s%10s%10s","1","ENG3C1-01\t","ENG3C1-01\n");
 
 	}
 
