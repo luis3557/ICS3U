a330ca10d7ccad181d5ef5168ba9f843eed62fcf
Luis Estrada <thelast3557@gmail.com>
2016-09-15 13:07:51 -0400

    Schedule

--- Aboutme/src/Estrada/Aboutme.java
+++ Aboutme/src/Estrada/Aboutme.java
@@ -4,7 +4,7 @@
 package Estrada;
 
 /**
- *IC3U
+ *ICS3U
  *Aboutme
  *Luis Estrada
  *September 12
@@ -18,7 +18,29 @@ public class Aboutme {
 		System.out.format("%10s","Luis.E\n");
 		System.out.format("%10s","Miss.K\n" );
 		System.out.format("%10s","Runnymede Collegiate Institute\n");
-		System.out.format("%10s","\"Go Ravens!/");
+		System.out.format("%10s","\"Go Ravens!/\n");
+
+		System.out.format("%-10s%8s%8s","Period","Day1","\tDay2\n");
+		System.out.format("%-10s%8s%8s","1","ENG3C1-01","\tENG3C1-01\n");
+		System.out.format("%-10s%8s%8s","8:45", "", "\n");
+		System.out.format("%-10s%8s%8s","10:02", "", "\n\n");
+		
+		System.out.format("%-10s%8s%8s","2","PAF3OM-01","\tPAF3OM-01\n");
+		System.out.format("%-10s%8s%8s","10:07", "", "\n");
+		System.out.format("%-10s%8s%8s","11:24", "", "\n\n");
+		
+		System.out.format("%-10s%8s%8s","Lunch","Lunch","\tLunch\n");
+		System.out.format("%-10s%8s%8s","11:24", "", "\n");
+		System.out.format("%-10s%8s%8s","12:26", "", "\n\n");
+		
+		System.out.format("%-10s%8s%8s","3","ICS3C1-01","\tICS3C1-01\n");
+		System.out.format("%-10s%8s%8s","12:26", "", "\n");
+		System.out.format("%-10s%8s%8s","13:43", "", "\n\n");
+		
+		System.out.format("%-10s%8s%8s","4","SNC2D1-03","\tSNC2D1-03\n");
+		System.out.format("%-10s%8s%8s","13:48", "", "\n");
+		System.out.format("%-10s%8s%8s","15:5", "", "\n\n");
+	
 	}
 
 }
---
+++ Aboutme/src/Estrada/ClassSchedule.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ * Luis Estrada
+ * ICS3U
+ * ClassSchedule
+ *Sep/14/2016
+ */
+public class ClassSchedule {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-10s%-12s%-10s","Period","Day1","Day2\n");
+		System.out.format("%-8s%-12s%-12s","1","ENG3C1-01\t","ENG3C1-01\n");
+
+	}
+
+}
