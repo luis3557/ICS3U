a14088a5c0680a11a75fde8278b227bb3a7302af
Luis Estrada <thelast3557@gmail.com>
2016-09-19 13:37:46 -0400

    JavaTerminology

---
+++ Aboutme/src/Estrada/JavaTerminology.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ *Luis Estrada
+ * ICS3U
+ * ClassSchedule
+ *Sep/19/2016
+ */
+public class JavaTerminology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-10s","Algorithm A set of steps that outline how to solve a problem\t\n\n");
+		System.out.format("%-10s","Argument Data passed to a method for processing.\t\n\n");
+		System.out.format("%-10s","Class Tje description of an object\t\n\n");
+		System.out.format("%-10s","Comment Text that provides infromation to the reader of program,class,or method\t\n\n");
+		System.out.format("%-10s","OPP see Obeject-oriented pprogramming.\t\n\n");
+
+	}
+
+}
