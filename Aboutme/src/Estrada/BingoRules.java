1dd0e608af3a8ca7186aa568bf6e9e2405b0e05e
Luis Estrada <thelast3557@gmail.com>
2016-09-19 13:06:27 -0400

    BingoRules LuisEstrada

---
+++ Aboutme/src/Estrada/BingoRules.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ *Luis Estrada
+ * ICS3U
+ * ClassSchedule
+ *Sep/15/2016
+ */
+
+public class BingoRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%10s","Bingo Card\n\n");
+		System.out.format("%-10s","1. Bingo caller randomly pulls a numbere bingo ball.\t\n\n");
+		
+		System.out.format("%-10s","2. The number is placed on the bingo board and called out\t\n\n");
+		
+		System.out.format("%-10s","3. Players look for the called number on their bingo card.\t\n\n");
+		
+		System.out.format("%-10s","4. if the number is located, it is marked off.\t\n\n");
+		
+		System.out.format("%-10s","5. Steps 1 to 4 are rapeated until a player matches the BINGO pattern.\t\n\n");
+
+		System.out.format("%-10s","6.The winning player yelss Bingo\t\n\n");
+
+
+
+
+	}
+
+}
