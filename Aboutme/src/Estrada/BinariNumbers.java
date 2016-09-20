92bccf771449475cd832519ce5af321027c86f3c
Luis Estrada <thelast3557@gmail.com>
2016-09-20 13:02:14 -0400

    Unit1

---
+++ Aboutme/src/Estrada/BinariNumbers.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Estrada;
+
+/**
+ * @author 344169792
+ *
+ */
+public class BinariNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.format("%10s%10s","Decimal","Binary\t\n");
+		System.out.format("%10s%10s","1","1\t\n");
+		System.out.format("%10s%10s","2","10\t\n");	
+		System.out.format("%10s%10s","3","11\t\n");
+		System.out.format("%10s%10s","4","100\t\n");
+		System.out.format("%10s%10s","5","101\t\n");
+		System.out.format("%10s%10s","6","110\t\n");
+		System.out.format("%10s%10s","7","111\t\n");
+		System.out.format("%10s%10s","8","1000\t\n");
+		System.out.format("%10s%10s","9","1001\t\n");
+		System.out.format("%10s%10s","10","1010\t\n");
+		System.out.format("%10s%10s","11","11011\t\n");
+		System.out.format("%10s%10s","12","1100\t\n");
+		
+		
+		
+	}
+
+}
--- Aboutme/src/Estrada/JavaTerminology.java
+++ Aboutme/src/Estrada/JavaTerminology.java
@@ -17,7 +17,7 @@ public class JavaTerminology {
 	public static void main(String[] args) {
 		System.out.format("%-10s","Algorithm A set of steps that outline how to solve a problem\t\n\n");
 		System.out.format("%-10s","Argument Data passed to a method for processing.\t\n\n");
-		System.out.format("%-10s","Class Tje description of an object\t\n\n");
+		System.out.format("%-10s","Class The description of an object\t\n\n");
 		System.out.format("%-10s","Comment Text that provides infromation to the reader of program,class,or method\t\n\n");
 		System.out.format("%-10s","OPP see Obeject-oriented pprogramming.\t\n\n");
 
