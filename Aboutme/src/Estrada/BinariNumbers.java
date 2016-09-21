0e44909551d4c0317059ce0b89ef14347d6c4e29
Luis Estrada <thelast3557@gmail.com>
2016-09-21 12:42:19 -0400

    Unit1

--- Aboutme/src/Estrada/BinariNumbers.java
+++ Aboutme/src/Estrada/BinariNumbers.java
@@ -15,6 +15,7 @@ public class BinariNumbers {
 	public static void main(String[] args) {
 		
 		System.out.format("%10s%10s","Decimal","Binary\t\n");
+		
 		System.out.format("%10s%10s","1","1\t\n");
 		System.out.format("%10s%10s","2","10\t\n");	
 		System.out.format("%10s%10s","3","11\t\n");
@@ -27,6 +28,48 @@ public class BinariNumbers {
 		System.out.format("%10s%10s","10","1010\t\n");
 		System.out.format("%10s%10s","11","11011\t\n");
 		System.out.format("%10s%10s","12","1100\t\n");
+		System.out.format("%10s%10s","13","1101\t\n");	
+		System.out.format("%10s%10s","14","1110\t\n");
+		System.out.format("%10s%10s","15","1111\t\n");
+		System.out.format("%10s%10s","16","10000\t\n");
+		System.out.format("%10s%10s","17","10001\t\n");
+		System.out.format("%10s%10s","18","10010\t\n");
+		System.out.format("%10s%10s","19","10011\t\n");
+		System.out.format("%10s%10s","20","100000\t\n\n");
+		
+		
+		System.out.format("%2s%15s%15s%18s","Decimal","Binary","Decimal","Haxadecimal\t\n");
+		
+		System.out.format("%2s%15s%15s%18s","1","1","1","0\t\n");
+		System.out.format("%2s%15s%15s%18s","2","10","2","1\t\n");
+		System.out.format("%2s%15s%15s%18s","3","11","3","2\t\n");
+		System.out.format("%2s%15s%15s%18s","4","100","4","3\t\n");
+		System.out.format("%2s%15s%15s%18s","5","101","5","4\t\n");
+		System.out.format("%2s%15s%15s%18s","6","110","6","5\t\n");
+		System.out.format("%2s%15s%15s%18s","7","111","7","6\t\n");
+		System.out.format("%2s%15s%15s%18s","8","1000","8","7\t\n");
+		System.out.format("%2s%15s%15s%18s","9","1001","9","8\t\n");
+		System.out.format("%2s%15s%15s%18s","10","1010","10","9\t\n");
+		System.out.format("%2s%15s%15s%18s","11","1011","11","A\t\n");
+		System.out.format("%2s%15s%15s%18s","12","10000","12","B\t\n");
+		System.out.format("%2s%15s%15s%18s","13","10001","13","C\t\n");
+		System.out.format("%2s%15s%15s%18s","14","10010","14","D\t\n");
+		System.out.format("%2s%15s%15s%18s","15","10011","15","E\t\n");
+		System.out.format("%2s%15s%15s%18s","16","100000","16","F\t\n");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+		
 		
 		
 		
