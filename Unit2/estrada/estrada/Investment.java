8f7069aae374c3d574c58a55d37237f2c3a4b671
Luis Estrada <thelast3557@gmail.com>
2016-11-24 13:24:21 -0500

    Investment

---
+++ Unit2/estrada/estrada/Investment.java
@@ -0,0 +1,48 @@
+package estrada;
+
+public class Investment {
+
+
+	/**
+	 *Luis Estrada
+	 *Investment.java
+	 *November 23
+	 *	This determines how many years it would take for $2500 to turn into $5000 if 
+	 *compounded at 7.5% annually.
+	 */
+
+	public Investment() {
+
+	}
+
+	public static void main(String[] args) {
+		System.exit(0);
+
+		int investment = 2500;
+		double maximum = 5000;
+
+
+		double investment1=maximum;
+		double years = 1;
+
+
+		while 
+			(investment<=maximum){
+			years = years +1;
+			double maximun;
+			maximum =investment1 *1.075;
+
+			System.out.println("It will take:)"+ years + ("years to compuded at 7.5% annually"));
+
+		}
+
+	}
+
+
+
+
+
+}                                  
+
+
+                                                                                                                                                                                                                       
