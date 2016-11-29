7ba6f525a1e8ba26b3dfde8357408a2a6a6b59b2
Luis Estrada <thelast3557@gmail.com>
2016-11-29 13:16:12 -0500

    RemoveString

---
+++ Unit2/estrada/estrada/RemoveString.java
@@ -0,0 +1,37 @@
+package estrada;
+
+import java.util.Scanner;
+
+/**
+ *Luis Estrada
+ *PrimerFactor.java
+ *November 23
+ *	 *
+ */
+public class RemoveString {
+
+	public static void main(String[] args) {
+	
+		Scanner scan=new Scanner(System.in);
+		
+		String sentence;
+		String string;
+		
+		
+		
+		System.out.println("Enter a sentence");
+		sentence=scan.nextLine();
+		System.out.println("Enter a string");
+		string=scan.nextLine();
+			
+			System.out.println(sentence.replace(string, " "));
+			
+		
+		  // System.outprintln(sentence.charAt(string));
+   
+		
+			System.out.println("I hope you get and intierview.");
+
+	}
+
+}
