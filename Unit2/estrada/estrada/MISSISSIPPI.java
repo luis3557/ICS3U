f515284443773d53221b8d5b2700aa853bbbf0d3
344169792 <344169792@C-5LQJHS1.tdsb.on.ca>
2017-01-12 13:00:53 -0500

    Mississippi

---
+++ Unit2/estrada/estrada/MISSISSIPPI.java
@@ -0,0 +1,89 @@
+package estrada;
+
+public class MISSISSIPPI {
+
+	public MISSISSIPPI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		drawM();
+		drawI();
+		drawS();
+		drawS();
+		drawI();
+		drawS();
+		drawS();
+		drawI();
+		drawP();
+		drawP();
+		drawI();
+	}
+
+	public static void drawM(){
+		System.out.println("M        M");
+		System.out.println("M M      M");
+		System.out.println("M   M M  M");		              
+		System.out.println("M    M   M");
+		System.out.println("M        M");
+		System.out.println("M        M");
+		System.out.println("M        M");
+
+	}
+	public static void drawI(){
+		System.out.println("   IIIII ");
+		System.out.println("     I   ");
+		System.out.println("     I   ");
+		System.out.println("     I   ");
+		System.out.println("     I   ");
+		System.out.println("     I   ");
+		System.out.println("   IIIII   ");
+	}
+
+
+	public static void drawS () {
+
+		System.out.println("  SSSSSS   ");
+		System.out.println(" S     S     ");
+		System.out.println(" S       ");
+		System.out.println(" S         ");
+		System.out.println("   SSSSSS   ");
+		System.out.println("         S  ");
+		System.out.println("S        S ");
+		System.out.println("  SSSSSSS ");
+
+
+
+
+	}
+
+
+	public static void drawP(){
+		System.out.println(" PPPPPPPPPP  ");
+		System.out.println("P          P ");
+		System.out.println("P         P");
+		System.out.println("P PPPPPPPP   ");
+		System.out.println("P       ");
+		System.out.println("P       ");
+		System.out.println("P       ");
+		System.out.println("P       ");
+
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
